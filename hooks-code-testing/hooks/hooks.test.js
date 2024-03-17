b3cf30dc083d4cdad6d6e9acd186465cfb723027
Taylor Lo <taylorloph@gmail.com>
2024-03-17 14:11:33 -0400

    Import hooks testing project

---
+++ hooks-code-testing/hooks/hooks.test.js
@@ -0,0 +1,33 @@
+import { it, expect } from "vitest";
+
+import { User } from "./hooks";
+
+const testEmail = "test@test.com";
+const user = new User(testEmail);
+
+it("should update the email", () => {
+  const newTestEmail = "test2@test.com";
+
+  user.updateEmail(newTestEmail);
+
+  expect(user.email).toBe(newTestEmail);
+});
+
+it("should have an email property", () => {
+  expect(user).toHaveProperty("email");
+});
+
+it("should store the provided email value", () => {
+  expect(user.email).toBe(testEmail);
+});
+
+it("should clear the email", () => {
+  user.clearEmail();
+  expect(user.email).toBe("");
+});
+
+it("should still have an email property after clearing the email", () => {
+  user.clearEmail();
+
+  expect(user).toHaveProperty("email");
+});
