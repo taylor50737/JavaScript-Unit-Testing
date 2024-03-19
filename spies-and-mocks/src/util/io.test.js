c096056165a8d6771a124d68cedf005466209962
Taylor Lo <taylorloph@gmail.com>
2024-03-18 22:24:56 -0400

    Mocks & Automocking

--- spies-and-mocks/src/util/io.test.js
+++ spies-and-mocks/src/util/io.test.js
@@ -1,9 +1,16 @@
-import { it, expect } from "vitest";
-import writeData from "./io";
+import { it, expect, vi } from "vitest";
+import { promises as fs } from "fs";
+
+import writeData from "./io.js";
+
+vi.mock("fs");
 
 it("should execute the writeFile method", () => {
   const testData = "Test";
   const testFilename = "test.txt";
-  
-  return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
+  writeData(testData, testFilename);
+
+  // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  expect(fs.writeFile).toBeCalled();
 });
